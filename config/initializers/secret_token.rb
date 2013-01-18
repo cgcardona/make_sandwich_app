f8ae777dc443a8c6cff795fa098eb0a9c038d95e
Carlos Cardona <cgcardona@gmail.com>
2013-01-18 13:15:45 -0800

    stub rails app with haml as template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeSandwichApp::Application.config.secret_token = '0c9d7dce329c24b0039ae982f70cdd8d915b1ce0e012f12d923ca786abc94224765d6ab14fbfaa694776ebb9ae67130a9984be026d6a91ab521e645d7e6521b4'

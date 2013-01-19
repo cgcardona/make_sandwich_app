12e18cabf49b6d8246b7a9ab08bc0d99b60fd961
Carlos Cardona <cgcardona@gmail.com>
2013-01-19 09:12:58 -0800

    properly build and send email based on sandwich preferences

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,4 +2,6 @@ class WelcomeMailer < ActionMailer::Base
   def send_sandwich_request(from, to, sandwichType, ingredients)
     @sandwich_type = sandwichType 
     @ingredients = ingredients
-    mail( :from => from, :to => "Carlos Cardona <" + to + ">", :subject => "Favor Pleeese :-]") end end
+    mail( :from => from, :to => "Carlos Cardona <" + to + ">", :subject => "Favor Pleeese :-]") 
+  end 
+end
